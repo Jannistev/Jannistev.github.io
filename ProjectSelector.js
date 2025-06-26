f8374f0d3335c2534e536c1882ad001876996a7f
Jannis Tev <jteveluwe01@student.rocvantwente.nl>
2025-06-27 00:56:18 +0200

    feat: updated projects
    updated projects

--- ProjectSelector.js
+++ ProjectSelector.js
@@ -2,25 +2,59 @@ let engine = document.getElementById("engine");
 let device = document.getElementById("device");
 const cards = document.querySelectorAll(".card");
 
-engine.addEventListener('change', CardSelector);
-device.addEventListener('change', CardSelector);
+engine.addEventListener('change', ShowCards);
+device.addEventListener('change', ShowCards);
 
-function CardSelector() {
+ShowCards();
 
-    const selectedEngine = engine.value;
-    const selectedDevice = device.value;
+function ShowCards() {
 
-    for (let i = 0; i < cards.length; i++) {
-        const cardTags = cards[i].getAttribute('data-tags');
+const selectedEngine = engine.value;
+const selectedDevice = device.value;
+const projectContainer = document.getElementById("ProjectsContainer")
 
-        console.log(selectedEngine);
-        console.log(selectedDevice);
-        console.log(cardTags);
+projectContainer.innerHTML = "";
 
-        if (cardTags.includes(selectedEngine) && cardTags.includes(selectedDevice)) {
-            cards[i].style.display = "block";
-        } else {
-            cards[i].style.display = "none";
+for (let i = 0; i < data.length; i++) {
+
+    if (!data[i].tags.includes(selectedEngine) || !data[i].tags.includes(selectedDevice))
+        continue;
+    
+    const projectCard = document.createElement("div");
+    projectCard.className = "col-12 col-lg-4";
+
+    let carouselItems = [];
+
+    for (let j = 0; j < data[i].images.length; j++) {
+        let img = data[i].images[j];
+        let active = "";
+        
+        if (j === 0) {
+            active = "active";
         }
+
+        carouselItems += `
+        <div class="carousel-item ${active}">
+            <img src="${img}" class="d-block img-fluid" alt="${data[i].title}_Img${j + 1}" style="height: 340px; width:100%;">
+        </div>
+    `;
     }
+
+    projectCard.innerHTML = `
+    <div class="card mt-5 border-secondary" style="width: 100%; padding: 5px; min-height: 520px;" data-tags="${data[i].tags}">
+        <div id="${data[i].carouselId}" class="carousel slide" data-bs-ride="carousel" data-bs-pause="false">
+            <div class="carousel-inner">
+               ${carouselItems}
+            </div>
+        </div>
+        <div class="card-body text-center">
+            <h5 class="card-title"><b>${data[i].title}</b></h5>
+            <p class="card-text">${data[i].description}</p>
+            <a href="${data[i].link}" class="btn btn-primary" target="_blank">Lees meer</a>
+        </div>
+    </div>
+`;
+
+projectContainer.appendChild(projectCard);
+}
 }
